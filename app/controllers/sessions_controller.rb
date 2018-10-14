81a79839d0b7b6f562d08800b2e75b73bea9ed4c
Everett <everett.bowline@gmail.com>
2018-10-14 17:02:34 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,12 @@ class SessionsController < ApplicationController
         redirect_to login_path
       else
         session[:name] = params[:name]
-        puts "Hi, #{params[:name]}"
+        puts "Hi, #{params[:name]}."
         redirect_to root_path
       end
     end
 
     def destroy
-      session.delete :username
+      session.delete :name
     end
 end
